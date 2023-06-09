23eb268d08229f0ffc8227f374f0548bd0b34ed7
shoxbb <nljshoxbb@gmail.com>
2023-06-09 11:24:04 +0000

    update packages/pc-single-component-template/projectTemplate/publishSyncrc.js.
    Signed-off-by: shoxbb <nljshoxbb@gmail.com>

--- packages/pc-single-component-template/projectTemplate/publishSyncrc.js
+++ packages/pc-single-component-template/projectTemplate/publishSyncrc.js
@@ -1,14 +1,17 @@
 module.exports = {
   api: {
+    origin: 'http://localhost:3200',
     syncInfo: {
-      path: 'http://localhost:3200/v1/package/sync',
-      method: 'POST'
+      path: '/v1/package/sync',
+      method: 'POST',
     },
     upload: {
-      path: "http://localhost:3200/v1/file/upload",
-      method: "POST"
-    }
+      path: '/v1/file/upload',
+      method: 'POST',
+    },
   },
-  zipSrc: "build",
-  zipName: "docs",
-}
+  zipSrc: 'build',
+  zipName: 'docs',
+  /** 管理员分配token */
+  token: '',
+};
